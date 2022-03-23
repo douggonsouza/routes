9425e54bce5c064171a79a2e4a16e093c45348e6
Douglas Gonçalves de Souza <douglas.goncalves@grupofolha.com.br>
2022-03-23 08:23:33 -0300

    Composer instalado

---
+++ vendor/composer/installed.php
@@ -0,0 +1,34 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '5b419bd1a105a708032eefe0efeedae69dd4e7fc',
+        'name' => 'douggonsouza/routes',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'douggonsouza/regexed' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../douggonsouza/regexed',
+            'aliases' => array(
+                0 => '9999999-dev',
+            ),
+            'reference' => '433f175d6a3c2de69d021e15f496cd78610f4315',
+            'dev_requirement' => false,
+        ),
+        'douggonsouza/routes' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '5b419bd1a105a708032eefe0efeedae69dd4e7fc',
+            'dev_requirement' => false,
+        ),
+    ),
+);
